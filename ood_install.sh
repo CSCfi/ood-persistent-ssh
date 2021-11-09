b26b43fbe41d304c83a10c504fab8cfbcbc0d656
Henrik Nortamo <hnortamo@csc.fi>
2021-11-09 14:22:57 +0000

    Update ood_install.sh

--- ood_install.sh
+++ ood_install.sh
@@ -1,3 +1,3 @@
 ln -fns ../../../deps/util/forms/form_validated.js form.js
-cp ssh_wrapper.sh ../../../deps/ssh_wrapper.sh
-chmod +x ../../../deps/ssh_wrapper.sh
+cp ssh_wrapper.sh ../../../deps/ssh_wrapper
+chmod +x ../../../deps/ssh_wrapper
