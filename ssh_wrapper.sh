b4114a45a1a5f09413051c3495ae0b180c695c20
Robin Karlsson <robin.karlsson@csc.fi>
2023-10-04 18:21:53 +0300

    Use  instead of

--- ssh_wrapper.sh
+++ ssh_wrapper.sh
@@ -9,14 +9,14 @@ tmux_path=/appl/local/ood/$ood_instance/soft/tmux/bin/
 export TERM=xterm-256color
 
 if [[ -z "$(echo "$@" | grep '^lumi\|^193\|^uan'  )" ]]; then
-    export SLURM_JOB_ID="$(squeue --me --nodelist="$*" --noheader --format="%i" --name='sys/dashboard/sys/ood-persistent-ssh,sys/dashboard/dev/ood-persistent-ssh' | head -n 1)"
+    export SLURM_JOB_ID="$(squeue --me --nodelist="$1" --noheader --format="%i" --name='sys/dashboard/sys/ood-persistent-ssh,sys/dashboard/dev/ood-persistent-ssh' | head -n 1)"
 
     if [[ -n "$SLURM_JOB_ID" ]];then
       # SSH to compute node (persistent)
-      /usr/bin/ssh "$login_host" -tt srun --overlap --jobid="$SLURM_JOB_ID" --nodelist="$*" --chdir "$HOME" test -f "$tmux_path/tmux" &>/dev/null
+      /usr/bin/ssh "$login_host" -tt srun --overlap --jobid="$SLURM_JOB_ID" --nodelist="$1" --chdir "$HOME" test -f "$tmux_path/tmux" &>/dev/null
 
       if [[ $? -eq 0 ]];then
-          /usr/bin/ssh "$login_host" -tt srun --pty --overlap --jobid="$SLURM_JOB_ID" --nodelist="$*" --chdir "$HOME" "$(dirname "$tmux_path")/start_tmux.sh"
+          /usr/bin/ssh "$login_host" -tt srun --pty --overlap --jobid="$SLURM_JOB_ID" --nodelist="$1" --chdir "$HOME" "$(dirname "$tmux_path")/start_tmux.sh"
       else
           RED='\033[0;31m'
           NC='\033[0m'
@@ -27,12 +27,12 @@ if [[ -z "$(echo "$@" | grep '^lumi\|^193\|^uan'  )" ]]; then
           else
               echo "SSH wrapper failed, executable $tmux_path/tmux does not exist" | logger
           fi
-          /usr/bin/ssh "$login_host" -tt srun --pty --overlap --jobid="$SLURM_JOB_ID" --nodelist="$*" --chdir "$HOME" "$SHELL"
+          /usr/bin/ssh "$login_host" -tt srun --pty --overlap --jobid="$SLURM_JOB_ID" --nodelist="$1" --chdir "$HOME" "$SHELL"
       fi
     else
       # SSH to compute node (non-persistent)
-      export SLURM_JOB_ID="$(squeue --me --nodelist="$*" --noheader --format="%i" | head -n 1)"
-      /usr/bin/ssh "$login_host" -tt srun --pty --overlap --jobid="$SLURM_JOB_ID" --nodelist="$*" --chdir "$HOME" "$SHELL"
+      export SLURM_JOB_ID="$(squeue --me --nodelist="$1" --noheader --format="%i" | head -n 1)"
+      /usr/bin/ssh "$login_host" -tt srun --pty --overlap --jobid="$SLURM_JOB_ID" --nodelist="$1" --chdir "$HOME" "$SHELL"
     fi
 else
     # SSH to login node
