e132be52750277c4589b9886b7cf28b2e0084054
Robin Karlsson <robin.karlsson@csc.fi>
2023-10-04 18:06:55 +0300

    Add SSH to compute nodes
    Used for SSHing to nodes for running jobs, not persistent shell.

--- ssh_wrapper.sh
+++ ssh_wrapper.sh
@@ -11,22 +11,30 @@ export TERM=xterm-256color
 if [[ -z "$(echo "$@" | grep '^lumi\|^193\|^uan'  )" ]]; then
     export SLURM_JOB_ID="$(squeue --me --nodelist="$*" --noheader --format="%i" --name='sys/dashboard/sys/ood-persistent-ssh,sys/dashboard/dev/ood-persistent-ssh' | head -n 1)"
 
-    /usr/bin/ssh "$login_host" -tt srun --overlap --jobid="$SLURM_JOB_ID" --nodelist="$*" --chdir "$HOME" test -f "$tmux_path/tmux" &>/dev/null
+    if [[ -n "$SLURM_JOB_ID" ]];then
+      # SSH to compute node (persistent)
+      /usr/bin/ssh "$login_host" -tt srun --overlap --jobid="$SLURM_JOB_ID" --nodelist="$*" --chdir "$HOME" test -f "$tmux_path/tmux" &>/dev/null
 
-    if [[ $? -eq 0 ]];then
-        /usr/bin/ssh "$login_host" -tt srun --pty --overlap --jobid="$SLURM_JOB_ID" --nodelist="$*" --chdir "$HOME" "$(dirname "$tmux_path")/start_tmux.sh"
+      if [[ $? -eq 0 ]];then
+          /usr/bin/ssh "$login_host" -tt srun --pty --overlap --jobid="$SLURM_JOB_ID" --nodelist="$*" --chdir "$HOME" "$(dirname "$tmux_path")/start_tmux.sh"
+      else
+          RED='\033[0;31m'
+          NC='\033[0m'
+
+          echo -e "[${RED}INTERNAL ERROR${NC}] tmux binary not found.\n\tNo persistent session created\n\tPlease contact the CSC service desk" >&2
+          if [[ -z "$ood_instance" ]];then
+              echo "SSH wrapper failed, failed to resolve OOD instance CSC_OOD_ENVIRONMENT empty" | logger
+          else
+              echo "SSH wrapper failed, executable $tmux_path/tmux does not exist" | logger
+          fi
+          /usr/bin/ssh "$login_host" -tt srun --pty --overlap --jobid="$SLURM_JOB_ID" --nodelist="$*" --chdir "$HOME" "$SHELL"
+      fi
     else
-        RED='\033[0;31m'
-        NC='\033[0m'
-
-        echo -e "[${RED}INTERNAL ERROR${NC}] tmux binary not found.\n\tNo persistent session created\n\tPlease contact the CSC service desk" >&2
-        if [[ -z "$ood_instance" ]];then
-            echo "SSH wrapper failed, failed to resolve OOD instance CSC_OOD_ENVIRONMENT empty" | logger
-        else
-            echo "SSH wrapper failed, executable $tmux_path/tmux does not exist" | logger
-        fi
-        /usr/bin/ssh "$login_host" -tt srun --pty --overlap --jobid="$SLURM_JOB_ID" --nodelist="$*" --chdir "$HOME" "$SHELL"
+      # SSH to compute node (non-persistent)
+      export SLURM_JOB_ID="$(squeue --me --nodelist="$*" --noheader --format="%i" | head -n 1)"
+      /usr/bin/ssh "$login_host" -tt srun --pty --overlap --jobid="$SLURM_JOB_ID" --nodelist="$*" --chdir "$HOME" "$SHELL"
     fi
 else
-   /usr/bin/ssh $@
+    # SSH to login node
+    /usr/bin/ssh $@
 fi
