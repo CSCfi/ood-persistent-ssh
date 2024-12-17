8ab8d3a7df160f1dc90c2905f2feb041441efb6c
Robin Karlsson <robin.karlsson@csc.fi>
2024-12-17 09:48:33 +0200

    Disable password auth and retry once on failure
    Hostbased auth fails randomly. Work around that by retrying once if it
    fails.

--- ssh_wrapper.sh
+++ ssh_wrapper.sh
@@ -7,6 +7,31 @@ login_host=$(yq read /etc/ood/config/clusters.d/lumi.yaml 'v2.login.host')
 # Default PS1. Need to set this manually as $(ppwd) (which sets terminal title) wont be set (by /etc/bash.bashrc) if DISPLAY is not set.
 PS1FIX='"\\[\$(ppwd)\\]\\u@\\h:\\w> "'
 
+test_tmux() {
+  /usr/bin/ssh -oPasswordAuthentication=no -oKbdInteractiveAuthentication=no -oChallengeResponseAuthentication=no -tt "$login_host" srun --overlap --jobid="$SLURM_JOB_ID" --nodelist="$node" test -f "$tmux_path/tmux" &>/dev/null
+}
+
+start_tmux_session() {
+  /usr/bin/ssh -oPasswordAuthentication=no -oKbdInteractiveAuthentication=no -oChallengeResponseAuthentication=no -tt "$login_host" "cd $HOME; srun --pty --overlap --jobid='$SLURM_JOB_ID' --nodelist='$node' '$(dirname "$tmux_path")/start_tmux.sh'"
+}
+
+ssh_node_nonpersistent() {
+  /usr/bin/ssh -oPasswordAuthentication=no -oKbdInteractiveAuthentication=no -oChallengeResponseAuthentication=no -tt "$login_host" "cd $HOME; env PS1=$PS1FIX srun --pty --overlap --jobid='$SLURM_JOB_ID' --nodelist='$node' '$SHELL'"
+}
+
+ssh_login_host() {
+  /usr/bin/ssh -oPasswordAuthentication=no -oKbdInteractiveAuthentication=no -oChallengeResponseAuthentication=no "$@"
+}
+
+retry_once() {
+  "$@"
+  if [[ $? -eq 255 ]]; then
+    echo "SSH failed, trying again"
+    sleep 0.1
+    "$@"
+  fi
+}
+
 if [ "$#" -eq 1 ]; then
   # SSH to login node, no directory specified
   # Unset PWD to default to $HOME instead of full path to /pfs/lustrep/$HOME.
@@ -18,23 +43,24 @@ fi
 
 ood_instance=$SLURM_OOD_ENV
 tmux_path=/appl/local/ood/$ood_instance/soft/tmux/bin/
+node="$1"
 export TERM=xterm-256color
 
-if [[ -z "$(echo "$1" | grep '^lumi\|^193\|^uan'  )" ]]; then
+if [[ -z "$(echo "$node" | grep '^lumi\|^193\|^uan' )" ]]; then
 
     # Job id specified in URL.
     job_id=$(echo "$3" | sed "s#^cd '/\\([[:digit:]]\\+\\).*\$#\\1#; t; q1")
     if [ $? -eq 0 ]; then
       export SLURM_JOB_ID="$job_id"
     else
-      export SLURM_JOB_ID="$(squeue --me --nodelist="$1" --noheader --format="%i" --name='sys/dashboard/sys/ood-persistent-ssh,sys/dashboard/dev/ood-persistent-ssh' | head -n 1)"
+      export SLURM_JOB_ID="$(squeue --me --nodelist="$node" --noheader --format="%i" --name='sys/dashboard/sys/ood-persistent-ssh,sys/dashboard/dev/ood-persistent-ssh' | head -n 1)"
     fi
 
     if [[ -n "$SLURM_JOB_ID" ]];then
       # SSH to compute node (persistent)
-      /usr/bin/ssh "$login_host" -tt srun --overlap --jobid="$SLURM_JOB_ID" --nodelist="$1" test -f "$tmux_path/tmux" &>/dev/null
+      retry_once test_tmux
       if [[ $? -eq 0 ]];then
-          /usr/bin/ssh "$login_host" -tt "cd $HOME; srun --pty --overlap --jobid='$SLURM_JOB_ID' --nodelist='$1' '$(dirname "$tmux_path")/start_tmux.sh'"
+        retry_once start_tmux_session
       else
           RED='\033[0;31m'
           NC='\033[0m'
@@ -45,18 +71,18 @@ if [[ -z "$(echo "$1" | grep '^lumi\|^193\|^uan'  )" ]]; then
           else
               echo "SSH wrapper failed, executable $tmux_path/tmux does not exist" | logger
           fi
-          /usr/bin/ssh "$login_host" -tt "cd $HOME; env PS1=$PS1FIX srun --pty --overlap --jobid='$SLURM_JOB_ID' --nodelist='$1' '$SHELL'"
+          retry_once ssh_node_nonpersistent
       fi
     else
       # SSH to compute node (non-persistent)
-      export SLURM_JOB_ID="$(squeue --me --nodelist="$1" --noheader --format="%i" | head -n 1)"
+      export SLURM_JOB_ID="$(squeue --me --nodelist="$node" --noheader --format="%i" | head -n 1)"
       if [[ -n "$SLURM_JOB_ID" ]];then
-        /usr/bin/ssh "$login_host" -tt "cd $HOME; env PS1=$PS1FIX srun --pty --overlap --jobid='$SLURM_JOB_ID' --nodelist='$1' '$SHELL'"
+        retry_once ssh_node_nonpersistent
       else
-        echo "No job found on node $1"
+        echo "No job found on node $node"
       fi
     fi
 else
     # SSH to login node
-    /usr/bin/ssh $@
+    retry_once ssh_login_host "$@"
 fi
