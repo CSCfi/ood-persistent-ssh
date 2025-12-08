70782b42523b1ace3058a61a3c4b013177d5265b
Robin Karlsson <robin.karlsson@csc.fi>
2025-12-08 15:43:34 +0200

    Run tmux in job script instead
    tmux server exits when SSH command exits if run with srun. Simplest
    solution is to run tmux in the job script and keep it alive for the
    whole duration of the job rather than re-launching it from  the SSH
    wrapper each time.

--- start_tmux.sh
+++ start_tmux.sh
@@ -3,22 +3,12 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 export TMPDIR=/tmp/$USER/$SLURM_JOB_ID
-
 test -d "/run/nvme/job_$SLURM_JOB_ID/tmp/" && export TMPDIR=/run/nvme/job_$SLURM_JOB_ID/tmp
-mkdir -p "$TMPDIR"
-mkdir -p $TMPDIR/tmux
-chmod og-rwx -R $TMPDIR/tmux
 export TMUX_TMPDIR=$TMPDIR/tmux
 
-
-test -f "/tmp/$USER/$SLURM_JOB_ID/persist_ssh" && \
+export PATH="$SCRIPT_DIR/bin:$PATH"
 {
-  export PATH="$SCRIPT_DIR/bin:$PATH"
-  test -f "/tmp/$USER/$SLURM_JOB_ID/custom_tmux_conf" && export _CSC_TMUX_CONF="" \
-  || export _CSC_TMUX_CONF="-f <(echo -e \"set -g status off\nsetw -g mouse on\nset -g set-titles on\nset -g set-titles-string 'Compute node shell (#h)'\")"
-  {
-      tmux ls 2>/dev/null && "$SCRIPT_DIR/time_helper.sh" &>/dev/null & tmux attach -t "$SLURM_JOB_ID" &>/dev/null
-  } \
-  || eval "tmux $_CSC_TMUX_CONF new-session -s $SLURM_JOB_ID"
-} || bash
+    tmux has-session -t "$SLURM_JOB_ID" 2>/dev/null && "$SCRIPT_DIR/time_helper.sh" &>/dev/null & tmux attach -t "$SLURM_JOB_ID" &>/dev/null
+} \
+|| eval "tmux -N new-session -s $SLURM_JOB_ID"
 
