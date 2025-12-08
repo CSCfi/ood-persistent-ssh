bcd1d6308c245d2874e8af5d41839c860f1d1a13
Robin Karlsson <robin.karlsson@csc.fi>
2025-12-08 17:02:22 +0200

    Update install script to install tmux 3.6a

--- ood_install.sh
+++ ood_install.sh
@@ -1,8 +1,8 @@
 
 
-(cd $TMPDIR && wget https://github.com/tmux/tmux/releases/download/3.1c/tmux-3.1c.tar.gz && tar -xvf tmux-3.1c.tar.gz && cd tmux-3.1c  && ./configure --prefix=/appl/opt/ood/$1/soft/tmux && make && make install  )
-rm $TMPDIR/tmux-3.1c.tar.gz
-rm -r $TMPDIR/tmux-3.1c
+(cd $TMPDIR && wget https://github.com/tmux/tmux/releases/download/3.6a/tmux-3.6a.tar.gz && tar -xvf tmux-3.6a.tar.gz && cd tmux-3.6a  && ./configure --prefix=/appl/opt/ood/$1/soft/tmux && make && make install  )
+rm $TMPDIR/tmux-3.6a.tar.gz
+rm -r $TMPDIR/tmux-3.6a
 
 ln -fns ../../../deps/util/forms/form_validated.js form.js
 cp ssh_wrapper.sh ../../../deps/ssh_wrapper
