c89a0ec33b2a0bd1450cb5fd24723ae9e50584ab
Robin Karlsson <robin.karlsson@csc.fi>
2025-12-09 11:31:18 +0200

    Allow tmux to start new servers for now

--- start_tmux.sh
+++ start_tmux.sh
@@ -4,11 +4,14 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 export TMPDIR=/tmp/$USER/$SLURM_JOB_ID
 test -d "/run/nvme/job_$SLURM_JOB_ID/tmp/" && export TMPDIR=/run/nvme/job_$SLURM_JOB_ID/tmp
-export TMUX_TMPDIR=$TMPDIR/tmux
+export TMUX_TMPDIR="$TMPDIR/tmux"
+mkdir -p "$TMUX_TMPDIR"
+chmod og-rwx -R "$TMUX_TMPDIR"
 
-export PATH="$SCRIPT_DIR/bin:$PATH"
+test -f "$TMPDIR/custom_tmux_conf" && export _CSC_TMUX_CONF="" || export _CSC_TMUX_CONF="-f <(echo -e \"set -g status off\nsetw -g mouse on\nset -g exit-empty off\nset -g exit-unattached off\nset -g set-titles on\nset -g set-titles-string 'Compute node shell (#h)'\")"
+
+export PATH="/appl/opt/ood/test/soft/tmux/bin:$PATH"
 {
     tmux has-session -t "$SLURM_JOB_ID" 2>/dev/null && "$SCRIPT_DIR/time_helper.sh" &>/dev/null & tmux attach -t "$SLURM_JOB_ID" &>/dev/null
 } \
-|| eval "tmux -N new-session -s $SLURM_JOB_ID"
-
+|| eval "tmux $_CSC_TMUX_CONF new-session -s $SLURM_JOB_ID"
